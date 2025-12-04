2fc7fe575cea0b4beb598ce9e62aee87d3f0a65a
RSU TJS STEM <technologyjusticestudies@rsu.edu>
2025-12-04 10:42:56 -0800

    Update app_start_commented.py

--- app_start_commented.py
+++ app_start_commented.py
@@ -6,7 +6,7 @@ import random
 
 # --- 2. SET UP THE APP'S TITLE ---
 # This command draws the main title on our web page.
-st.title("jerome 💬➡️😎")
+st.title("jerome 💬➡️🚌🚌🚌🚌🚌🚌🚌🚌🚌🚌🚌🚌🚌🚌🚌🚌🚌🚌🚌🚌🚌🚌🚌🚌🚌🚌🚌🚌🚌🚌🚌🚌🚌🚌🚌😎")
 
 # --- NEW: Instructions Section ---
 with st.expander("👉 How to use this app"):
